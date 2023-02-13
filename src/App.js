005c32209f78c537e43fe3729f71fe7cc89f3d57
pandit <dhamdherepandit@gmail.com>
2023-02-13 18:14:39 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
    <>
    <div>
     <h1>Hello Pandit</h1>
+    <Navbar />
    </div>
    </>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+const Navbar = () => {
+  return (
+    <div className='font-bold'>
+    <h1 className='text-orange-500'>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
